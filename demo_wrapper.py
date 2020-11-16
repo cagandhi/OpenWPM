59bfd306272305696f8b4d2660e3656068c42621
Chintan Gandhi <cagandhi97@gmail.com>
2020-11-16 02:21:11 -0500

    single run for sites

--- demo_wrapper.py
+++ demo_wrapper.py
@@ -12,48 +12,49 @@ if not os.path.exists(time_path):
 	os.makedirs(time_path)
 
 sites_dir={'news': ['https://www.cnn.com','https://www.news.yahoo.com','https://www.washingtonpost.com'], 'shopping': [], 'sports': []}
-extensions_dir={'noextension': False, 'ghostery': False, 'disconnect': False, 'ublock-origin': False, 'https-everywhere': False}
+extensions_names=['noextension', 'ghostery', 'disconnect', 'ublock-origin', 'https-everywhere'] 
+# {'noextension': False, 'ghostery': False, 'disconnect': False, 'ublock-origin': False, 'https-everywhere': False}
 runs=1
 
 # loop over category
-for category, site_list in sites_dir.items():
+for category, sites in sites_dir.items():
 
 	# make category folder
 	category_path=time_path+'/'+category
 	if not os.path.exists(category_path):
 		os.makedirs(category_path)
 
-	# loop over site
-	for site in site_list:
+	# # loop over site
+	# for site in site_list:
 
-		filter_site='_'.join(site.split('.')[1:])
+	# 	filter_site='_'.join(site.split('.')[1:])
 
-		# make site folder
-		site_path=category_path+'/'+filter_site
-		if not os.path.exists(site_path):
-			os.makedirs(site_path)
+	# 	# make site folder
+	# 	site_path=category_path+'/'+filter_site
+	# 	if not os.path.exists(site_path):
+	# 		os.makedirs(site_path)
 
-		# loop over extensions
-		for extension, flag in extensions_dir.items():
+	# loop over extensions
+	for extension in extensions_names:
 
-			# make extension folder
-			extension_path=site_path+'/'+extension
-			if not os.path.exists(extension_path):
-				os.makedirs(extension_path)
+		# make extension folder
+		extension_path=category_path+'/'+extension
+		if not os.path.exists(extension_path):
+			os.makedirs(extension_path)
 
-			if extension=='noextension':
-				ext_flags={}
-			else:
-				ext_flags={extension: True}
+		if extension=='noextension':
+			ext_flags={}
+		else:
+			ext_flags={extension: True}
 
-			for run in range(runs):
+		for run in range(runs):
 
-				print('\n')
-				print(category+' - '+site+' - '+extension+' - '+str(run))
+			print('\n')
+			print(category+' - '+extension+' - '+str(run))
 
-				# make run folder
-				run_path=extension_path+'/run_'+str(run)
-				if not os.path.exists(run_path):
-					os.makedirs(run_path)
+			# make run folder
+			run_path=extension_path+'/run_'+str(run)
+			if not os.path.exists(run_path):
+				os.makedirs(run_path)
 
-				helper_run(run_path, [site], ext_flags)
+			helper_run(run_path, sites, ext_flags)
